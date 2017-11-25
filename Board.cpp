306e676204a7ccc1eb99cd8d0786509be5f4ce7b
Pratik Ghogale <pratik.ghogale@gmail.com>
2017-11-25 05:33:16 -0600

    no message

--- Board.cpp
+++ Board.cpp
@@ -9,86 +9,60 @@ Board::Board(){
         }
 }
 
-bool Board::isBoardFilled(){
- for (int i = 0; i<rows; i++)
-    {
-        for (int j = 0; j<columns; j++)
-        {
-            if(board[i][j]==' ')
-                return false;
-        }
-    }
-    return true;
-}
-
 int Board::checkIfPlayerWins(int player,int row,int column){
     char char_Player;
-    int returnValue;
     int count=1;
-    if(player==1){
+    if(player==1)
         char_Player='X';
-        returnValue=10;
-    }
-    else if(player==2){
+    else if(player==2)
         char_Player='O';
-        returnValue=-10;
-    }
     int r=row;
     int c=column;
     int tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
     bool left=true,right=true,up=true,down=true,leftUp=true,leftdown=true,rightUp=true,rightDown=true;
 
     //checking if we have winning row
-       while(count!=winCount){
+       while(count!=4){
             if((tempLeft-1)>=0 && left){
-                if(board[r][tempLeft-1]!=char_Player)
+                if(board[r][tempLeft-1]==char_Player)
+                    count++;
+                else
                    left=false;
                 tempLeft--;
             }
-            else
-                left=false;
-            if((tempRight+1)<=columns-1 && right){
-                if(board[r][tempRight+1]!=char_Player)
+            if((tempRight+1)<=5 && right){
+                if(board[r][tempRight+1]==char_Player)
+                    count++;
+                else
                    right=false;
-                tempRight++;
+                   tempRight++;
             }
-            else
-                right=false;
-            if(left && right)
-                count=count+2;
-            else
-                count++;
-            if(count==winCount && (left || right)){
-                return returnValue;
+            if(count==4 && (left || right)){
+                return 10;
             }
             if(!(left || right)){
                 break;
             }
        }
-       count=1;
 
        //checking if we have winning column
-        while(count!=winCount){
+        while(count!=4){
             if((tempUp-1)>=0 && up){
-                if(board[tempUp-1][c]!=char_Player)
+                if(board[tempUp-1][c]==char_Player)
+                    count++;
+                else
                    up=false;
                 tempUp--;
             }
-            else
-                up=false;
-            if((tempDown+1)<=rows-1 && down){
-                if(board[tempDown+1][c]!=char_Player)
+            if((tempDown+1)<=5 && down){
+                if(board[tempDown+1][c]==char_Player)
+                    count++;
+                else
                    down=false;
-                tempDown++;
+                   tempDown++;
             }
-            else
-                down=false;
-            if(up && down)
-                count=count+2;
-            else
-                count++;
-            if(count==winCount && (up || down)){
-                return returnValue;
+            if(count==4 && (up || down)){
+                return 10;
             }
             if(!(up || down)){
                 break;
@@ -97,29 +71,25 @@ int Board::checkIfPlayerWins(int player,int row,int column){
        tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
        count=1;
 
-       while(count!=winCount){
-            if((tempUp-1)>=0 && (tempRight+1)<=columns-1 && rightUp){
-                if(board[tempUp-1][tempRight+1]!=char_Player)
+       while(count!=4){
+            if((tempUp-1)>=0 && (tempRight+1)<=5 && rightUp){
+                if(board[tempUp-1][tempRight+1]==char_Player)
+                    count++;
+                else
                    rightUp=false;
                 tempUp--;
                 tempRight++;
             }
-            else
-                rightUp=false;
-            if((tempDown+1)<=rows-1 && (tempLeft-1)>=0 && leftdown){
-                if(board[tempDown+1][tempLeft-1]!=char_Player)
+            if((tempDown+1)<=5 && (tempLeft-1)>=0 && leftdown){
+                if(board[tempDown+1][tempLeft-1]==char_Player)
+                    count++;
+                else
                    leftdown=false;
                 tempDown++;
                 tempLeft--;
             }
-            else
-                leftdown=false;
-            if(rightUp && leftdown)
-                count=count+2;
-            else
-                count++;
-            if(count==winCount && (rightUp || leftdown)){
-                return returnValue;
+            if(count==4 && (rightUp || leftdown)){
+                return 10;
             }
             if(!(rightUp || leftdown)){
                 break;
@@ -130,76 +100,49 @@ int Board::checkIfPlayerWins(int player,int row,int column){
        tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
        count=1;
 
-       while(count!=winCount){
+       while(count!=4){
             if((tempUp-1)>=0 && (tempLeft-1)>0 && leftUp){
-                if(board[tempUp-1][tempLeft-1]!=char_Player)
+                if(board[tempUp-1][tempLeft-1]==char_Player)
+                    count++;
+                else
                    leftUp=false;
                 tempUp--;
                 tempLeft--;
             }
-            else
-                leftUp=false;
-            if((tempDown+1)<=rows-1 && (tempRight+1)<=columns-1 && rightDown){
-                if(board[tempDown+1][tempRight+1]!=char_Player)
+
+            if((tempDown+1)<=5 && (tempRight+1)<=5 && rightDown){
+                if(board[tempDown+1][tempRight+1]==char_Player)
+                    count++;
+                else
                    rightDown=false;
                 tempDown++;
                 tempRight++;
             }
-            else
-                rightDown=false;
-            if(leftUp && rightDown)
-                count=count+2;
-            else
-                count++;
-            if(count==winCount && (leftUp || rightDown)){
-                return returnValue;
+            if(count==4 && (leftUp || rightDown)){
+                return 10;
             }
             if(!(leftUp || rightDown)){
                 break;
             }
        }
        return 0;
-}
 
-bool Board::checkInsertRule(int x,int y){
-    if(x==rows-1){
-        return true;
-    }
-    else{
-        if(board[x+1][y]!=' ')
-            return true;
-    }
-    return false;
 }
 
 //performing the users move
-int Board::performMove(int x,int y){
+void Board::performMove(int x,int y){
     if(board[rows-1-x][y]==' '){
-        if(checkInsertRule(rows-1-x,y)){
-            board[rows-1-x][y]='X';
-            displayBoard();
-            int result=checkIfPlayerWins(1,rows-1-x,y);
-            if(result==0){
-                //getInput();
-                //displayBoard();
-            return 0;
-            }
-            else if(result==10){
-                cout<<"Player 1 wins"<<endl;
-                return 1;
-            }else if(result==-10){
-                cout<<"Player 2 wins"<<endl;
-            }
-        }
-        else{
-            cout<<"Cannot Insert in this position. Please try new position"<<endl;
-            return 2;
-        }
-
+        board[rows-1-x][y]='X';
+        displayBoard();
+        int result=checkIfPlayerWins(1,rows-1-x,y);
+        if(result==0)
+            getInput();
+        else
+            cout<<"Player 1 wins"<<endl;
     }
     else{
         cout<<"This position is already occupied. Please choose another position"<<endl;
-        return 2;
+        getInput();
     }
 }
 
@@ -222,22 +165,17 @@ void Board::displayBoard(){
 }
 
 //getting inputs from user for X and Y Co-ordinates
-int Board::getInput(){
+void Board::getInput(){
     cout<<"Enter row : ";
-   cin>>usersCurrent_RowNo;
+    cin>>usersCurrent_RowNo;
     cout<<"Enter column : ";
     cin>>usersCurrent_ColumnNo;
-//    board[3][0]='X';
-//    board[2][0]='0';
-//    board[1][0]='X';
-   // usersCurrent_RowNo=0;
-    //usersCurrent_ColumnNo=0;
-    return performMove(usersCurrent_RowNo,usersCurrent_ColumnNo);
+    performMove(usersCurrent_RowNo,usersCurrent_ColumnNo);
 }
 
-int Board::userPerformMove(int player){
-    //displayBoard();
-    return getInput();
+void Board::startGame(){
+    displayBoard();
+    getInput();
 }
 
 int Board::getRowNo(){
@@ -248,164 +186,6 @@ int Board::getColumnNo(){
     return usersCurrent_ColumnNo;
 }
 
-
-void Board::setValue(int x,int y,char c){
-    board[x][y]=c;
-}
-char Board::getValue(int x,int y){
-    return board[x][y];
-}
-
-
-int Board::evaluateFunction(int player,int row,int column){
-    char char_Player;
-    int returnValue;
-    int count1=1,count2=1,count3=1,count4=1;
-    if(player==1){
-        char_Player='X';
-        returnValue=10;
-    }
-    else if(player==2){
-        char_Player='O';
-        returnValue=-10;
-    }
-    int r=row;
-    int c=column;
-    int tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
-    bool left=true,right=true,up=true,down=true,leftUp=true,leftdown=true,rightUp=true,rightDown=true;
-
-    //checking if we have winning row
-       while(count1!=winCount){
-            if((tempLeft-1)>=0 && left){
-                if(board[r][tempLeft-1]!=char_Player)
-                   left=false;
-                tempLeft--;
-            }
-            else
-                left=false;
-            if((tempRight+1)<=columns-1 && right){
-                if(board[r][tempRight+1]!=char_Player)
-                   right=false;
-                tempRight++;
-            }
-            else
-                right=false;
-            if(left && right)
-                count1=count1+2;
-            else if(left || right)
-                count1++;
-            if(count1==winCount && (left || right)){
-                return returnValue;
-            }
-            if(!(left || right)){
-                break;
-            }
-       }
-       //count=1;
-
-       //checking if we have winning column
-        while(count2!=winCount){
-            if((tempUp-1)>=0 && up){
-                if(board[tempUp-1][c]!=char_Player)
-                   up=false;
-                tempUp--;
-            }
-            else
-                up=false;
-            if((tempDown+1)<=rows-1 && down){
-                if(board[tempDown+1][c]!=char_Player)
-                   down=false;
-                tempDown++;
-            }
-            else
-                down=false;
-            if(up && down)
-                count2=count2+2;
-            else if(up || down)
-                count2++;
-            if(count2==winCount && (up || down)){
-                return returnValue;
-            }
-            if(!(up || down)){
-                break;
-            }
-       }
-       tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
-       //count=1;
-
-       while(count3!=winCount){
-            if((tempUp-1)>=0 && (tempRight+1)<=columns-1 && rightUp){
-                if(board[tempUp-1][tempRight+1]!=char_Player)
-                   rightUp=false;
-                tempUp--;
-                tempRight++;
-            }
-            else
-                rightUp=false;
-            if((tempDown+1)<=rows-1 && (tempLeft-1)>=0 && leftdown){
-                if(board[tempDown+1][tempLeft-1]!=char_Player)
-                   leftdown=false;
-                tempDown++;
-                tempLeft--;
-            }
-            else
-                leftdown=false;
-            if(rightUp && leftdown)
-                count3=count3+2;
-            else if(rightUp || leftdown)
-                count3++;
-            if(count3==winCount && (rightUp || leftdown)){
-                return returnValue;
-            }
-            if(!(rightUp || leftdown)){
-                break;
-            }
-       }
-
-
-       tempLeft=c,tempRight=c,tempUp=r,tempDown=r;
-       //count=1;
-
-       while(count4!=winCount){
-            if((tempUp-1)>=0 && (tempLeft-1)>0 && leftUp){
-                if(board[tempUp-1][tempLeft-1]!=char_Player)
-                   leftUp=false;
-                tempUp--;
-                tempLeft--;
-            }
-            else
-                leftUp=false;
-            if((tempDown+1)<=rows-1 && (tempRight+1)<=columns-1 && rightDown){
-                if(board[tempDown+1][tempRight+1]!=char_Player)
-                   rightDown=false;
-                tempDown++;
-                tempRight++;
-            }
-            else
-                rightDown=false;
-            if(leftUp && rightDown)
-                count4=count4+2;
-            else if (leftUp || rightDown)
-                count4++;
-            if(count4==winCount && (leftUp || rightDown)){
-                return returnValue;
-            }
-            if(!(leftUp || rightDown)){
-                break;
-            }
-       }
-       int count= max(count1,count2);
-        count= max(count,count3);
-        count= max(count,count4);
-       // cout<<"Count1: "<<count1<<" ";
-        //cout<<"Count2: "<<count2<<" ";
-        //cout<<"Count3: "<<count3<<" ";
-        //cout<<"Max Count: "<<count<<" "<<endl;
-       if(player==2){
-          return -count;
-       }
-       return count;
-}
 
 
 
--- Board.h
+++ Board.h
@@ -1,13 +1,11 @@
 #ifndef BOARD_H_INCLUDED
 #define BOARD_H_INCLUDED
 
-
 class Board{
 private:
     char board[6][7];
     int rows=6;
     int columns=7;
-    int winCount=4;
     int playerX=1;
     int playerO=2;
     int noPlayer=0;
@@ -16,17 +14,14 @@ private:
 public:
     Board();
     int checkIfPlayerWins(int,int,int);
-    int performMove(int,int);
+    void performMove(int,int);
     void displayBoard();
-    int getInput();
-    int userPerformMove(int);
+    void getInput();
+    void startGame();
     int getRowNo();
     int getColumnNo();
-    bool checkInsertRule(int,int);
-    bool isBoardFilled();
-    void setValue(int,int,char);
-    char getValue(int,int);
-    int evaluateFunction(int,int,int);
+
 };
+
 
 #endif // BOARD_H_INCLUDED
