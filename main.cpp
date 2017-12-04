c0c3ce7828849949c776fab7b4bf00a849f7b25c
pratikg11 <pratik.ghogale@gmail.com>
2017-12-04 07:02:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,126 +1,94 @@
 #include<iostream>
 #include"Board.h"
 #include"Board.cpp"
-#include"Ai_Player.h"
-#include"Ai_Player.cpp"
+#include"AI_Player.h"
+#include"AI_Player.cpp"
 #include <time.h>
 #include <stdio.h>
-//#include"psapi.h"
 using namespace std;
 
+int main() {
+	int noOfNodes = 0;
+	int countminus = 0;
+	int pathLength = 0;
+	int choice, totalPossibleMoves = 42;
+	int evaluateFunctionNo=1;
+	int count = 0;
+	bool result = true;
+	Board b;
+	Ai_Player a;
+	//Selecting the algorithm
+	do {
+		cout << "Select 1 choice" << endl;
+		cout << "1.Minimax Algorithm" << endl;
+		cout << "2.Minimax using alpha beta pruning" << endl;
+		cin >> choice;
+		if (choice <= 0 || choice >= 3)
+			cout << "Invalid Choice.. Please Select Proper Choice" << endl;
+	} while (choice <= 0 || choice >= 3);
 
-int main()
-{
-    clock_t tStart = clock();
-    int noOfNodes=0;
-    int choice,totalPossibleMoves=42;
-    int count=0;
-    bool result=true;
-    Board b;
-    Ai_Player a;
-    char c='y';
-    cout<<"Select 1 choice"<<endl;
-    cout<<"1.Minimax Algorithm"<<endl;
-    cout<<"2.Minimax using alpha beta pruning"<<endl;
-    cin>>choice;
-   // choice=2;
-    //cout<<"Do u want to start 1st with X(Y/N)";
-   // cin>>c;
-//b.setValue(5,0,'X');
-//b.setValue(5,1,'X');
-//   b.setValue(5,2,'O');
-//   b.setValue(5,3,'O');
-//   b.setValue(5,4,'O');
-//   b.setValue(5,5,'X');
-//   b.setValue(5,6,'X');
-//   b.setValue(4,0,'O');
-//   b.setValue(4,1,'X');
-//   b.setValue(4,2,'X');
-//   b.setValue(4,3,'O');
-//   b.setValue(4,4,'X');
-//   b.setValue(4,5,'O');
-//   b.setValue(4,6,'O');
-//   b.setValue(3,1,'X');
-//   b.setValue(2,1,'O');
-//////    if(c=='y'){
-//////            while(result)
-//////            {
-//////                b.displayBoard();
-//////               int i=b.userPerformMove(1);
-//////               //int i=b.setValue(3,2,'X');
-//////                if(i==1){
-//////                    result=false;
-//////                }
-//////                else if(i==2)
-//////                    result=true;
-////////                else if(i==0){
-////////                    int j=a.AIperformMove(b,choice,2,noOfNodes);
-////////                    b.displayBoard();
-////////                    if(j==-10)
-////////                    {
-////////                       b.displayBoard();
-////////                       cout<<"Player 2 wins";
-////////                       result=false;
-////////                    }
-////////
-////////                }
-//////                count=count+2;
-//////                if(count==totalPossibleMoves)
-//////                {
-//////                    cout<<"Game is Over. Its a draw"<<endl;
-//////                    result=false;
-//////                }
-//////            }
-//////
-//////    }
+	//Setting start time
+	clock_t tStart = clock();
+	while (result) {
+		pathLength++;
+		int j = a.AIperformMove(b, choice, 1, noOfNodes, countminus,
+				evaluateFunctionNo);
+		cout << endl << "Player 1 move : ";
+		b.displayBoard();
+		if (j == 10) {
+			cout << "Player 1 wins";
+			result = false;
+		}
+		if (result) {
+			pathLength++;
+			int k = a.AIperformMove(b, choice, 2, noOfNodes, countminus,
+					evaluateFunctionNo);
+			cout << endl << "Player 2 move : ";
+			b.displayBoard();
+			if (k == -10) {
+				cout << "Player 2 wins";
+				result = false;
+			}
 
+			count = count + 2;
+			if (j != 10 && k != -10) {
+				if (count == totalPossibleMoves) {
+					cout << "Game is Over. Its a draw" << endl;
+					result = false;
+				}
+			}
+		}
 
-while(result)
-{
-    int j=a.AIperformMove(b,choice,1,noOfNodes);
-    cout<<endl<<"Player 1 move : ";
-     b.displayBoard();
-                    if(j==10)
-                    {
-                       b.displayBoard();
-                       cout<<"Player 1 wins";
-                       result=false;
-                    }
-        if(result){
-        int k=a.AIperformMove(b,choice,2,noOfNodes);
-        cout<<endl<<"Player 2 move : ";
-        b.displayBoard();
-                    if(k==-10)
-                    {
-                       b.displayBoard();
-                       cout<<"Player 2 wins";
-                       result=false;
-                    }
+	}
+	cout << endl << "No of Nodes Generated is : " << noOfNodes << endl;
+	cout << "No of Nodes Expanded : " << noOfNodes - countminus << endl;
+	if (choice == 1) {
+		if (evaluateFunctionNo == 1) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 149) + 143 << " Bytes" << endl;
+		} else if (evaluateFunctionNo == 2) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 165) + 143 << " Bytes" << endl;
+		} else if (evaluateFunctionNo == 3) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 145) + 143 << " Bytes" << endl;
+		}
+	} else if (choice == 2) {
 
-    count=count+2;
-                if(j!=10 && k!=-10){
-                if(count==totalPossibleMoves)
-                {
-                    cout<<"Game is Over. Its a draw"<<endl;
-                    result=false;
-                }
-                }
-}
+		if (evaluateFunctionNo == 1) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 150) + 143 << " Bytes" << endl;
+		} else if (evaluateFunctionNo == 2) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 166) + 143 << " Bytes" << endl;
+		} else if (evaluateFunctionNo == 3) {
+			cout << "The size of memory used by the program is : "
+					<< (noOfNodes * 146) + 143 << " Bytes" << endl;
+		}
+	}
+	cout << "The total length of the game path is : " << pathLength << endl;
+//the total time for program
+	cout << "Time taken is : " << (double) (clock() - tStart) / CLOCKS_PER_SEC
+			<< " seconds" << endl;
 
-}
-cout<<endl<<"No of Nodes Generated : "<<noOfNodes<<endl;
-cout<<"No of Ticks: "<< (double)(clock() - tStart)<<endl;
-cout<<"Clocks per sec: "<<CLOCKS_PER_SEC<<endl;
-cout<<"Time taken: "<< (double)(clock() - tStart)/CLOCKS_PER_SEC<<endl;
-
-
-
-
-//PROCESS_MEMORY_COUNTERS memCounter;
-//bool result2 = GetProcessMemoryInfo(GetCurrentProcess(),
-//                                   &memCounter,
-//                                   sizeof( memCounter ));
-//cout<<result2;
-   // else
-      //  a.AIperformMove(b);
 }
